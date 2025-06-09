a04a8021eb7486a5535e087270d25939ea66a0fe
Vitor Mesquita <abcvitormesquita@gmail.com>
2025-06-09 16:14:35 -0300

    vai uma vez

--- src/components/animated-background.tsx
+++ src/components/animated-background.tsx
@@ -14,3 +14,29 @@ import { useRouter } from "next/navigation";
 gsap.registerPlugin(ScrollTrigger);
 
 const STATES = {
+  hero: {
+    desktop: {
+      scale: { x: 0.25, y: 0.25, z: 0.25 },
+      position: { x: 400, y: -200, z: 0 },
+      rotation: { x: 0, y: 0, z: 0 },
+    },
+    mobile: {
+      scale: { x: 0.15, y: 0.15, z: 0.15 },
+      position: { x: 0, y: -200, z: 0 },
+      rotation: { x: 0, y: 0, z: 0 },
+    },
+  },
+  skills: {
+    desktop: {
+      scale: { x: 0.4, y: 0.4, z: 0.4 },
+      position: { x: 0, y: -40, z: 0 },
+      rotation: {
+        x: 0,
+        y: Math.PI / 12,
+        z: 0,
+      },
+    },
+    mobile: {
+      scale: { x: 0.2, y: 0.2, z: 0.2 },
+      position: { x: 0, y: -40, z: 0 },
+      rotation: {
