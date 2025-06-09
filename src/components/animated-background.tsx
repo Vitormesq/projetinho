9c75cb8ef025873e188a844966e99317fd1a5893
Vitor Mesquita <abcvitormesquita@gmail.com>
2025-06-09 16:20:11 -0300

    eae vey n aguento mais

--- src/components/animated-background.tsx
+++ src/components/animated-background.tsx
@@ -40,3 +40,107 @@ const STATES = {
       scale: { x: 0.2, y: 0.2, z: 0.2 },
       position: { x: 0, y: -40, z: 0 },
       rotation: {
+        x: 0,
+        y: Math.PI / 6,
+        z: 0,
+      },
+    },
+  },
+  projects: {
+    desktop: {
+      scale: { x: 0.3, y: 0.3, z: 0.3 },
+      position: { x: 0, y: -40, z: 0 },
+      rotation: {
+        x: Math.PI,
+        y: Math.PI / 3,
+        z: Math.PI,
+      },
+    },
+    mobile: {
+      scale: { x: 0.18, y: 0.18, z: 0.18 },
+      position: { x: 0, y: 150, z: 0 },
+      rotation: {
+        x: Math.PI,
+        y: Math.PI / 3,
+        z: Math.PI,
+      },
+    },
+  },
+  contact: {
+    desktop: {
+      scale: { x: 0.3, y: 0.3, z: 0.3 },
+      position: { x: 500, y: -250, z: 0 },
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+    },
+    mobile: {
+      scale: { x: 0.18, y: 0.18, z: 0.18 },
+      position: { x: 0, y: 150, z: 0 },
+      rotation: {
+        x: Math.PI,
+        y: Math.PI / 3,
+        z: Math.PI,
+      },
+    },
+  },
+};
+
+type Section = "hero" | "skills" | "projects" | "contact";
+
+const AnimatedBackground = () => {
+  const { isLoading, bypassLoading } = usePreloader();
+  const { theme } = useTheme();
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  const splineContainer = useRef<HTMLDivElement>(null);
+  const [splineApp, setSplineApp] = useState<Application>();
+
+  if (isMobile) {
+    return null;
+  }
+
+  const [activeSection, setActiveSection] = useState<Section>("hero");
+
+  const keyboardStates = (section: Section) => {
+    return STATES[section][isMobile ? "mobile" : "desktop"];
+  };
+
+  useEffect(() => {
+    if (splineApp) {
+      const kbd = splineApp.findObjectByName("keyboard");
+      if (kbd) {
+        gsap.to(kbd.scale, {
+          ...keyboardStates(activeSection).scale,
+          duration: 1,
+        });
+        gsap.to(kbd.position, {
+          ...keyboardStates(activeSection).position,
+          duration: 1,
+        });
+        gsap.to(kbd.rotation, {
+          ...keyboardStates(activeSection).rotation,
+          duration: 1,
+        });
+      }
+    }
+  }, [activeSection, splineApp, isMobile]);
+
+  return (
+    <>
+      <Suspense fallback={<div>Carregando...</div>}>
+        <Spline
+          ref={splineContainer}
+          onLoad={(app: Application) => {
+            setSplineApp(app);
+            bypassLoading();
+          }}
+          scene="/assets/skills-keyboard.spline"
+        />
+      </Suspense>
+    </>
+  );
+};
+
+export default AnimatedBackground;
